b91480b022e6568049a662e486e586d51007b5b3
Ahmad Nassri <ahmad@codeinchaos.com>
2015-05-03 20:52:25 -0400

    full coverage + enhanced jade.filters usage

--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@
 
 var debug = require('debug')('metalsmith-jade')
 var jade = require('jade')
-var jade_filters = require('jade/lib/filters')
-var jade_runtime = require('jade/lib/runtime')
 var path = require('path')
 var util = require('util')
 
-/**
- * Metalsmith plugin to convert jade files.
- *
- * @param {Object} options (optional)
- *   @property {Array} keys
- * @return {Function}
- */
-
-function plugin (options) {
-  options = options || {}
-  options.locals = options.locals || {}
+module.exports = function (options) {
+  var opts = options || {}
+  opts.locals = opts.locals || {}
 
   return function (files, metalsmith, done) {
     setImmediate(done)
 
-    if (options.useMetadata) {
-      options.locals = util._extend(options.locals, metalsmith.metadata())
+    // extend with metalsmith global metadata
+    if (opts.useMetadata) {
+      opts.locals = util._extend(opts.locals, metalsmith.metadata())
     }
 
     Object.keys(files).forEach(function (file) {
@@ -37,12 +28,10 @@ function plugin (options) {
       var dir = path.dirname(file)
       var name = path.basename(file, path.extname(file))
 
-      // do need to add an extension
+      // do we need to add an extension?
       if (path.extname(name) === '') {
         name = path.basename(file, path.extname(file)) + '.html'
       }
-
-      var locals = options.locals
 
       if (dir !== '.') {
         name = dir + '/' + name
@@ -50,31 +39,31 @@ function plugin (options) {
 
       debug('converting file: %s', file)
 
-      options.filename = path.join(metalsmith.source(), file)
+      opts.filename = path.join(metalsmith.source(), file)
 
-      if (options.useMetadata) {
-        locals = util._extend(options.locals, data)
+      // also use the file's own data
+      if (opts.useMetadata) {
+        opts.locals = util._extend(opts.locals, data)
       }
 
-      var str = jade.compile(data.contents.toString(), options)(locals)
+      // assign filters
+      if (opts.filters) {
+        Object.keys(opts.filters).forEach(function (filter) {
+          jade.filters[filter] = opts.filters[filter]
+        })
+      }
 
+      // render
+      var str = jade.compile(data.contents.toString(), opts)(opts.locals)
+
+      // convert to a buffer
       data.contents = new Buffer(str)
 
+      // remove from global files object
       delete files[file]
 
+      // assign the newly created file
       files[name] = data
     })
   }
 }
-
-/**
- * Register a filter with jade using a factory method which returns the actual
- * filter method
- */
-plugin.registerFilter = function (name, factory) {
-  jade_filters[name] = factory(jade, jade_runtime, jade_filters)
-}
-
-// Expose `plugin`
-
-module.exports = plugin
---
+++ test/index.js
@@ -0,0 +1,153 @@
+/* global afterEach, describe, it */
+'use strict'
+
+var del = require('del')
+var fs = require('fs')
+var jade = require('..')
+var Metalsmith = require('metalsmith')
+var should = require('should')
+
+afterEach(function (done) {
+  del('test/*/build', done)
+})
+
+describe('metalsmith-jade', function () {
+  it('should render html', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.use(jade())
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.exists('test/main/build/index.html', function (exists) {
+        exists.should.be.true
+
+        done()
+      })
+    })
+  })
+
+  it('should render files within directories', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.use(jade())
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.exists('test/main/build/dir/test.html', function (exists) {
+        exists.should.be.true
+
+        done()
+      })
+    })
+  })
+
+  it('should render html with locals', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.use(jade({
+      locals: {
+        title: 'Foo'
+      }
+    }))
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.readFile('test/main/build/locals.html', function (err, data) {
+        should.not.exist(err)
+        data.toString().should.equal('<h1>Foo</h1>')
+        done()
+      })
+    })
+  })
+
+  it('should use Metalsmith.metadata()', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.metadata({
+      foo: 'bar'
+    })
+
+    smith.use(jade({
+      useMetadata: true
+    }))
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.readFile('test/main/build/metadata.html', function (err, data) {
+        should.not.exist(err)
+        data.toString().should.equal('<h1>bar</h1>')
+
+        done()
+      })
+    })
+  })
+
+  it('should use extension prefix as extension', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.use(jade({
+      locals: {
+        foo: 'bar'
+      }
+    }))
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.exists('test/main/build/text.txt', function (exists) {
+        exists.should.be.true
+
+        fs.readFile('test/main/build/text.txt', function (err, data) {
+          should.not.exist(err)
+          data.toString().should.equal('bar')
+
+          done()
+        })
+      })
+    })
+  })
+
+  it('should not run on none .jade files', function (done) {
+    var smith = new Metalsmith('test/main')
+
+    smith.use(jade())
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.exists('test/main/file.html', function (exists) {
+        exists.should.be.false
+
+        done()
+      })
+    })
+  })
+
+  it('should register jade filters', function (done) {
+    var smith = new Metalsmith('test/filters')
+
+    smith.use(jade({
+      filters: {
+        foo: function (block) {
+          return block
+        }
+      }
+    }))
+
+    smith.build(function (err) {
+      should.not.exist(err)
+
+      fs.readFile('test/filters/build/test.html', function (err, data) {
+        should.not.exist(err)
+        data.toString().should.equal('bar')
+
+        done()
+      })
+    })
+  })
+})
